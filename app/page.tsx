6da48cb1f74cb6f7c5568f78ca377958efe273f1
Luong Dang <luongkhdang@gmail.com>
2024-11-12 02:13:46 -0800

    fix loginbutton, add z-10

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     return (
         <div className="relative min-h-screen flex items-center justify-center bg-green-50">
             {/* Login Button positioned at the top-right */}
-            <div className="absolute top-4 right-4">
+            <div className="absolute top-4 right-4 z-20">
                 <LoginButton />
             </div>
 
             {/* Background Image */}
-            <div className="relative w-full h-full min-w-[80vw] min-h-[85vh] md:w-[50vw] md:h-[50vh]  ">
+            <div className="relative w-full h-full min-w-[80vw] min-h-[85vh] md:w-[50vw] md:h-[50vh]  z-10 ">
                 <Image
                     src="/images/background1.PNG"
                     alt="Background floral shop"
